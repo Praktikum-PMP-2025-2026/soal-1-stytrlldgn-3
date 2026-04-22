b35f7eb8435b8a8fdd91b67287fdcde049bdbccc
stytrlldgn <satyaadnyanaq@gmail.com>
2026-04-22 10:35:06 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -9,14 +9,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int find_max_in_array(int arr[], int size) {
-    int max_val = arr[0];
+void find_max_in_array(int arr[], int size, int *idx, int *max_val) {
+    *max_val = arr[0];
+    *idx = 0;
     for (int i = 1; i < size; i++) {
-        if (arr[i] > max_val) {
-            max_val = arr[i];
+        if (arr[i] > *max_val) {
+            *max_val = arr[i];
+            *idx = i;
         }
     }
-    return max_val;
 }
 
 int getSum(int arr[], int n) {
@@ -28,28 +29,23 @@ int getSum(int arr[], int n) {
 }
 
 int main(){
-    int n;
+    int n, indeks, max;
     scanf("%d",&n);
     int values[n];
     for (int k = 0; k<n;k++){
         scanf("%d",&values[k]);
     }
     
-    int max_value = find_max_in_array(values,n);
+    find_max_in_array(values,n, &indeks, &max);
     int res = getSum(values, n);
     float avg = (float)res/n;
 
+
     printf("SUM %d\n", res);
     printf("AVG %.2f\n",avg);
-    printf("MAX %d\n", max_value);
+    printf("MAX %d\n", max);
+    printf("IDX %d\n", indeks);
 
-    for (int i = 0; i<n; i++){
-        if (values[i]==max_value){
-            if (values[i]== values[i+1]){
-                printf("IDX %d\n",i);
-            }
-        }
-    }
     
     return 0;
 }
